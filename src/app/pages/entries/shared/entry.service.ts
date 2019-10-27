6366348db5298fdae8245e863ae893387a13f3f9
luizhcarvalho <luiz.carvalho@gmail.com>
2019-10-27 13:35:45 -0300

    Improving BaseResourceService and EntryService and CategoryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -12,8 +12,11 @@ import { Entry } from './entry.model';
 })
 export class EntryService extends BaseResourceService<Entry> {
 
-  constructor(protected injector: Injector, private categoryService: CategoryService) {
-    super('api/entries', injector);
+  constructor(
+    protected injector: Injector,
+    private categoryService: CategoryService
+  ) {
+    super('api/entries', injector, Entry.fromJson);
   }
 
   create(entry: Entry): Observable<Entry> {
@@ -32,22 +35,6 @@ export class EntryService extends BaseResourceService<Entry> {
         return super.update(entry);
       })
     );
-  }
-
-  // PROTECTED methods
-  protected jsonDataToResources(jsonData: any[]): Entry[] {
-    const entries: Entry[] = [];
-
-    jsonData.forEach(element => {
-      const entry = Object.assign(new Entry(), element);
-      entries.push(entry);
-    });
-
-    return entries;
-  }
-
-  protected jsonDataToResource(jsonData: any): Entry {
-    return Object.assign(new Entry(), jsonData);
   }
 
 }
